e98ceb35f4b96506e9e34be2067ca582f289e2c3
harshitawasthi <awasthiharshit40@gmail.com>
2022-10-02 23:36:44 +0530

    routes setup

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from './NavBar'
+import { Products } from './Products'
+
+export const Home = () => {
+    return (
+        <>
+            <Navbar/>
+            <Products/>
+        </>
+    )
+}
